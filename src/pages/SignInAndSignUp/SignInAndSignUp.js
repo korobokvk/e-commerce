052b1d4a2592ab95adf5b1af06d7303972a3ddcf
Maksym Zakshevskyi <mzakshevskyi@lohika.com>
2019-09-04 00:03:43 +0300

    added sign in page

---
+++ src/pages/SignInAndSignUp/SignInAndSignUp.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import SignIn from '../../components/SignIn/SignIn'
+import './SignInAndSignUp.scss'
+
+const SignInAndSignUp = () => (
+  <div className="sign-in-and-sign-up">
+    <SignIn />
+  </div>
+)
+
+export default SignInAndSignUp
