6d028b14166f923f8c11a837a2a21eb292cdd84d
korobokvk <korobokvk@gmail.com>
2019-12-22 01:25:33 +0200

    Implement add items utils and upd reducer

--- src/components/CustomButton/CustomButton.js
+++ src/components/CustomButton/CustomButton.js
@@ -2,8 +2,8 @@ import React from 'react'
 import cn from 'classnames'
 import './CustomButton.scss'
 
-const CustomButton = ({ children, isGoogleSignIn, ...rest }) => (
-  <button className={cn('custom-button', { 'google-sign-in': isGoogleSignIn })} {...rest}>
+const CustomButton = ({ children, isGoogleSignIn, inverted, ...rest }) => (
+  <button className={cn('custom-button', { 'google-sign-in': isGoogleSignIn, inverted })} {...rest}>
     {children}
   </button>
 )
--- src/redux/cart/cartReducer.js
+++ src/redux/cart/cartReducer.js
@@ -1,19 +1,24 @@
 import { createAction, createReducer } from '../reduxHelpers/reduxHelpers'
+import { addItemToCart } from '../../utils/addItemToCart'
 
 export const NAME = 'cart'
 
 const initialState = {
   hidden: false,
+  cartItems: [],
 }
 export const types = {
   TOGGLE_CART_HIDDEN: `${NAME}/TOGGLE_CART_HIDDEN`,
+  ADD_ITEM: `${NAME}/ADD_ITEM`,
 }
 
 export const actions = {
   toggleCartHidden: createAction(types.TOGGLE_CART_HIDDEN),
+  addItem: createAction(types.ADD_ITEM),
 }
 export const handlers = {
   [types.TOGGLE_CART_HIDDEN]: (state) => ({ ...state, hidden: !state.hidden }),
+  [types.ADD_ITEM]: (state, { payload }) => ({ cartItems: addItemToCart(state.cartItems, payload) }),
 }
 
 export default createReducer(initialState, handlers)
