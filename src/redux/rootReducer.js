ab02640c32d29866860e54c139fd66e132f0ed2f
korobokvk <korobokvk@gmail.com>
2020-01-04 01:36:42 +0200

    Added persistance added directory reducer

--- src/redux/rootReducer.js
+++ src/redux/rootReducer.js
@@ -1,8 +1,19 @@
 import { combineReducers } from 'redux'
+import { persistReducer } from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
 import userReducer, { NAME as UserReducer } from './user/userReducer'
 import cartReducer, { NAME as CartReducer } from './cart/cartReducer'
+import directoryReducer, { NAME as DirectoryReducer } from './directory/directoryReducer'
+const persistConfig = {
+  key: 'root',
+  storage,
+  whiteList: [CartReducer],
+}
 
-export default combineReducers({
+const rootReducer = combineReducers({
   [UserReducer]: userReducer,
   [CartReducer]: cartReducer,
+  [DirectoryReducer]: directoryReducer,
 })
+
+export default persistReducer(persistConfig, rootReducer)
