89d2f56aeb599d53db5deaae781d52ec10c960db
korobokvk <korobokvk@gmail.com>
2020-01-05 03:12:14 +0200

    refactoring

--- src/components/MenuItem/MenuItem.js
+++ src/components/MenuItem/MenuItem.js
@@ -1,9 +1,10 @@
 import React from 'react'
 import { withRouter } from 'react-router-dom'
+import cn from 'classnames'
 import './MenuItem.scss'
 
 const MenuItem = ({ title, imageUrl, size, linkUrl, match, history }) => (
-  <div className={`${size} menu-item`} onClick={() => history.push(`${match.url}${linkUrl}`)}>
+  <div className={cn('menu-item', size)} onClick={() => history.push(`${match.url}${linkUrl}`)}>
     <div
       style={{
         backgroundImage: `url(${imageUrl})`,
